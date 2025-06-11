16dcc3c6391b6e84c0b47c3d1e2ef238e10f963d
이은석 <gles0319@naver.com>
2025-06-11 12:32:56 +0900

    충돌테스트 이은석

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -23,7 +23,7 @@ public class MemberController {
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!ㅁㄴㅇㄹ!");
 		System.out.println("팀원 2 : 김현수");
 		System.out.println("팀원 3 : 고해석");
 	}
