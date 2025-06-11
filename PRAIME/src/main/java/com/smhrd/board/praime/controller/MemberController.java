8741fc49c8eaec488e30d49116d5c60b35f369bf
HyeonSuJJang5 <luckysanata@gmail.com>
2025-06-11 14:57:02 +0900

    제모ㅛㄱ
    내용

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -4,8 +4,5 @@ public class MemberController {
 
 	public static void main(String[] args) {
 		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("4");
 	}
 }
