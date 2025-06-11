b40cd71fab2659d566479ac2bb53cc07357f5c05
이은석 <gles0319@naver.com>
2025-06-11 12:36:23 +0900

    Revert "충돌테스트 이은석"
    This reverts commit 16dcc3c6391b6e84c0b47c3d1e2ef238e10f963d.

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -3,7 +3,33 @@ package com.smhrd.board.praime.controller;
 public class MemberController {
 
 	public static void main(String[] args) {
+<<<<<<< master
 		String a = "abcfd";
 		System.out.println(a);
+=======
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("팀장 이름(나이) : 이은석(25)");
+		System.out.println("팀원(115151515) : 이현희");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("팀원 2 : 김현수");
+		System.out.println("팀원 3 : 고해석");
+>>>>>>> 16dcc3c 충돌테스트 이은석
 	}
 }
