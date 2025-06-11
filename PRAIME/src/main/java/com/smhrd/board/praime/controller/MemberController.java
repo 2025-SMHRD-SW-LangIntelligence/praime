87c0449b45a3b500597a5b76c3679a723164e4df
HyeonSuJJang5 <luckysanata@gmail.com>
2025-06-11 12:17:17 +0900

    0611 팀원2 김현수추가

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -5,5 +5,6 @@ public class MemberController {
 	public static void main(String[] args) {
 		System.out.println("팀장 이름 : 이은석");
 		System.out.println("팀원(1) : 이현희");
+		System.out.println("팀원 2 : 김현수");
 	}
 }
