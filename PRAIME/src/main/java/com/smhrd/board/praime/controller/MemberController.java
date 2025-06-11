1435aee173f0e2350b9479c5990e0e08161386bf
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 12:29:46 +0900

    Test : 충돌테스트

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -6,6 +6,9 @@ public class MemberController {
 		System.out.println("팀장 이름(나이) : 이은석(25)");
 		System.out.println("팀원(1) : 이현희");
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("팀원 2 : 김현수");
 		System.out.println("팀원 3 : 고해석");
 	}
