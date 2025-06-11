07ab5b42b58d94e371bad40588c2837b1a95cc6d
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 12:32:06 +0900

    Test : 충돌테스트!!1

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -3,8 +3,23 @@ package com.smhrd.board.praime.controller;
 public class MemberController {
 
 	public static void main(String[] args) {
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("팀장 이름(나이) : 이은석(25)");
 		System.out.println("팀원(115151515) : 이현희");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("충돌연습!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
