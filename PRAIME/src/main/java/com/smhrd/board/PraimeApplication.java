1a17bb69df4cd3c6cbe0d1a7f1f5cf8fafae5e50
이은석 <gles0319@naver.com>
2025-06-11 11:27:47 +0900

    0611/이은석 - 프로젝트 생성

---
+++ PRAIME/src/main/java/com/smhrd/board/PraimeApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraimeApplication.class, args);
+	}
+
+}
---
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -0,0 +1,8 @@
+package com.smhrd.board.praime.controller;
+
+public class MemberController {
+
+	public static void main(String[] args) {
+		System.out.println("팀장 이름 : 이은석");
+	}
+}
