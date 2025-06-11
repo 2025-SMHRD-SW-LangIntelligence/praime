0afcfc69ffc204c3ecb44386edc1b767e2c61c2b
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 13:56:59 +0900

    Test : 충돌테스트13

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -3,6 +3,9 @@ package com.smhrd.board.praime.controller;
 public class MemberController {
 
 	public static void main(String[] args) {
-
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
 	}
 }
