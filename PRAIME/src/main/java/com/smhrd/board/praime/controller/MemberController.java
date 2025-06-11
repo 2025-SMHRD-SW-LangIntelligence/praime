c188ab809b28aa372f51288ca23863acff44bdf2
ardor924 <106001182+ardor924@users.noreply.github.com>
2025-06-11 12:50:17 +0900

    we6t4

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -5,10 +5,10 @@ public class MemberController {
 	public static void main(String[] args) {
 
 		System.out.println("충돌");
+		System.out.println("충돌4545454");
 		System.out.println("충돌");
 		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
+		System.out.println("충돌353535");
 		System.out.println("충돌");
 		System.out.println("충돌236236236236236");
 		System.out.println("충돌");
