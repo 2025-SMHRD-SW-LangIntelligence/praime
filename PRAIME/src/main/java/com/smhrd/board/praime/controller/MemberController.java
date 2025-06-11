dab526c21eba245c97b3a50f20c680cafa7ed735
고해석 <wfos3241@hanmail.net>
2025-06-11 12:50:36 +0900

    11111

--- PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
+++ PRAIME/src/main/java/com/smhrd/board/praime/controller/MemberController.java
@@ -4,23 +4,6 @@ public class MemberController {
 
 	public static void main(String[] args) {
 
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충충돌돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌 시킵니다");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
-		System.out.println("충돌");
+
 	}
 }
